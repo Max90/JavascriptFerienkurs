2f1566dc09ddb794192642eec2e3006c63f8eba2
Max90 <maximilian.feigl@stud.uni-regensburg.de>
2014-09-17 15:31:02 +0200

    zahlenraten erstellt

--- Nullstelle/nullscript.js
+++ Nullstelle/nullscript.js
@@ -1,3 +1,11 @@
 /**
  * Created by max on 17.09.14.
  */
+function validateInput() {
+    var x = $("#x").val();
+    var y = $("#y").val();
+    var n = $('input[name=range-input]').val();
+    $("#range-output").text(n);
+    var result = (x - y) / n;
+    console.log(result);
+}
---
+++ Zahlenraten/zahlenscript.js
@@ -0,0 +1,19 @@
+/**
+ * Created by max on 17.09.14.
+ */
+$(function () {
+    $("#time-container").hide();
+    $("#restart-container").hide();
+
+    function submitListener() {
+
+    }
+
+    function selectChanged() {
+
+    }
+
+    function restartListener() {
+
+    }
+});
